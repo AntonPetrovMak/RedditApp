d209c6ffc46aa4e62c487a16965a8ff0efae9178
Anton Petrov <anton.petrov@innovecs.com>
2020-08-07 17:30:34 +0300

    init project

---
+++ RedditApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  RedditApp
+//
+//  Created by Petrov Anton on 07.08.2020.
+//  Copyright © 2020 apn. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    return true
+  }
+
+}
+
---
+++ RedditApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditApp
+//
+//  Created by Petrov Anton on 07.08.2020.
+//  Copyright © 2020 apn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  
+}
+
